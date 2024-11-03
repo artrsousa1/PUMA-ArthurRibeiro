f94e5fd55eaf509383901d093898818ee89ca061
artrsousa1 <artrsousa1@gmail.com>
2024-11-03 17:01:28 -0300

    fix: exporting server to use in tests

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,7 +1,9 @@
 require('dotenv').config();
 const app = require('./app');
-const port = process.env.PORT || 8000;
+const port = 8000;
 
-app.listen(port, () => {
+const server = app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`)
 })
+
+module.exports =  server;
