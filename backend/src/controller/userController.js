63b49c36f9a52baf2e0ca2bbecb46c38fe06db06
artrsousa1 <artrsousa1@gmail.com>
2024-11-03 00:34:23 -0300

    feat(backend): add controller to handle user operations

---
+++ backend/src/controller/userController.js
@@ -0,0 +1,56 @@
+const userManager = require('../manager/userManager');
+const { PrismaClientKnownRequestError } = require('@prisma/client');
+
+const userController = {
+    getUsers: async (req, res) => {
+        try {
+            const users = await userManager.getAllUsers();
+            res.status(200).json({ data: users });
+        } catch (error) {
+            res.status(500).json({ message: error.message });
+        }
+    },
+    addUser: async (req, res) => {
+        try {
+            const username = req.body.username;
+            const addedUser = await userManager.addUser(username);
+            res.status(201).json({ message: `Usuário ${addedUser.username} adicionado com sucesso.` });
+        } catch (error) {
+            if(error instanceof PrismaClientKnownRequestError) {
+                if(error.code === 'P2002') {
+                    return res.status(400).json({ message: 'Usuário já cadastrado.'})
+                }
+            }
+            res.status(error.cause).json({ message: error.message });
+        }
+    },
+    deleteUser: async (req, res) => {
+        try {
+            const username = req.params.username;
+            const deletedUser = await userManager.deleteUser(username);
+            res.status(200).json({ 
+                message: `Usuário ${deletedUser.username} deletado com sucesso.`
+            })
+        } catch (error) {
+            if(error instanceof PrismaClientKnownRequestError) {
+                if(error.code === 'P2025') {
+                    return res.status(404).json({ message: 'Usuário não encontrado.'})
+                }
+            }
+            res.status(500).json({ message: 'Internal server error.' });
+        }
+    },
+    favoriteUser: async (req, res) => {
+        try {
+            const username = req.params.username;
+            const { action, user } = await userManager.toggleFavorite(username);
+            res.status(200).json({
+                message: `Usuário ${user} ${action} com sucesso.`
+            })
+        } catch (error) {
+            res.status(500).json({ message: error.message });
+        }
+    }
+}
+
+module.exports = userController;
