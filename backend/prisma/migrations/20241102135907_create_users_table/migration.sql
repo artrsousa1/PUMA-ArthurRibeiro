f3418c54e8ac5a1dae3a8ff97dc3347aef9223d8
artrsousa1 <artrsousa1@gmail.com>
2024-11-03 00:36:27 -0300

    feat(db): add user model

---
+++ backend/prisma/migrations/20241102135907_create_users_table/migration.sql
@@ -0,0 +1,14 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "username" TEXT NOT NULL,
+    "picture_url" TEXT NOT NULL,
+    "profile_url" TEXT NOT NULL,
+    "name" TEXT,
+    "is_favorited" BOOLEAN NOT NULL DEFAULT false,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_username_key" ON "User"("username");
