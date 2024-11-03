e8c5cfefc0b10d2e5c207806af14ad3f8730a4b1
artrsousa1 <artrsousa1@gmail.com>
2024-11-03 00:39:40 -0300

    feat(backend): add user router

---
+++ backend/src/routes/userRouter.js
@@ -0,0 +1,11 @@
+const router = require('express').Router();
+
+const userController = require('../controller/userController');
+
+
+router.get('/users', async (req, res) => await userController.getUsers(req,res));
+router.post('/users', async (req, res) => await userController.addUser(req,res));
+router.delete('/users/:username', async (req, res) => await userController.deleteUser(req,res));
+router.patch('/users/:username', async (req, res) => await userController.favoriteUser(req,res));
+
+module.exports = router;
