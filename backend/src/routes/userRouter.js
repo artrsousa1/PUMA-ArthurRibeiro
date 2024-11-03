7936b075aa085df3bd0a73759ee634eea3d45bc7
artrsousa1 <artrsousa1@gmail.com>
2024-11-03 17:00:29 -0300

    fix: change route name to add /toggle-star

--- backend/src/routes/userRouter.js
+++ backend/src/routes/userRouter.js
@@ -6,6 +6,6 @@ const userController = require('../controller/userController');
 router.get('/users', async (req, res) => await userController.getUsers(req,res));
 router.post('/users', async (req, res) => await userController.addUser(req,res));
 router.delete('/users/:username', async (req, res) => await userController.deleteUser(req,res));
-router.patch('/users/:username', async (req, res) => await userController.favoriteUser(req,res));
+router.patch('/users/:username/toggle-star', async (req, res) => await userController.favoriteUser(req,res));
 
 module.exports = router;
